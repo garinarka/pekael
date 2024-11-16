1c465f393341982bda738449849fc4c102fca709
thejgdt <theaberchio@gmail.com>
2024-11-16 14:06:12 +0700

    student model & migration

---
+++ app/Models/Student.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'classroom_id',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'phone',
+        'address',
+    ];
+
+    protected $casts = [
+        'classroom_id' => 'integer',
+    ];
+
+    public function classroom(): BelongsTo
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+}
