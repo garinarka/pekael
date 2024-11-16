7131269cab283534edf07ceb1be827ad6c1f1f14
thejgdt <theaberchio@gmail.com>
2024-11-16 14:56:01 +0700

    initialize Filament admin panel student resources

---
+++ app/Filament/Resources/StudentResource.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\StudentResource\Pages;
+use App\Filament\Resources\StudentResource\RelationManagers;
+use App\Models\Student;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class StudentResource extends Resource
+{
+    protected static ?string $model = Student::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    protected static ?string $navigationGroup = 'Personal Data Managements';
+
+    protected static ?int $navigationSort = 2;
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+    public static function getNavigationBadgeColor(): string|array|null
+    {
+        return static::getModel()::count() > 150 ? 'warning' : 'primary';
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('Main Details')
+                    ->description('Student main details')
+                    ->schema([
+                        Forms\Components\Select::make('classroom_id')
+                            ->relationship('classroom', 'name')
+                            ->required()
+                            ->searchable()
+                            ->preload()
+                            ->native(false),
+                        Forms\Components\Group::make([
+                            Forms\Components\TextInput::make('first_name')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('middle_name')
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('last_name')
+                                ->maxLength(255),
+                        ])->columns(3),
+                        Forms\Components\Group::make([
+                            Forms\Components\TextInput::make('phone')
+                                ->type('phone')
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('address')
+                                ->required()
+                                ->maxLength(255),
+                        ])->columns(2)
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('classroom.name')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make('Main details')
+                    ->description('Student main details')
+                    ->schema([
+                        TextEntry::make('classroom.name'),
+                        TextEntry::make('name'),
+                        TextEntry::make('phone')
+                            ->placeholder('N/A'),
+                        TextEntry::make('address'),
+                    ])->columns(2)
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListStudents::route('/'),
+            'create' => Pages\CreateStudent::route('/create'),
+            'view' => Pages\ViewStudent::route('/{record}'),
+            'edit' => Pages\EditStudent::route('/{record}/edit'),
+        ];
+    }
+}
